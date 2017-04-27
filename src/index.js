45f85e7baa9550a2cce54705b6bf90378d683188
mattgerard <matt@matthewgerard.com>
2017-04-27 01:32:13 -0700

    updated fav

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './pages/home';
 import './styles/style.css';
+import './img/favicon.ico';
 ReactDOM.render(<App />, document.getElementById('app'));
 
 if (module.hot) {
